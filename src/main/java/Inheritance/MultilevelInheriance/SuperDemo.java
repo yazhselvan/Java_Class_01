31efbe8b6780a5668ef98d12884a2ab72cf83c1a
yazhselvan <yazhslvn89@gmail.com>
2026-03-29 16:55:41 +0530

    Added super demo

---
+++ src/main/java/Inheritance/MultilevelInheriance/SuperDemo.java
@@ -0,0 +1,34 @@
+package Inheritance.MultilevelInheriance;
+
+/*
+Explain Super and Sub class --> Inheritance
+Parent and Child Class
+
+All normal class created in java are derived from "Object" class
+ */
+
+
+
+class A{
+    public A(){
+
+        System.out.println("in A");
+    }
+
+    public A(int a){
+        this(); //this will call the other constructor inside the same class
+        System.out.println("int A in");
+    }
+}
+
+class B extends A{
+    public B(){
+        super(1);
+        System.out.println("in B");
+    }
+}
+public class SuperDemo {
+    public static void main(String[] args) {
+        B obj = new B();
+    }
+}
